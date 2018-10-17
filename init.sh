dc9d3df8260cadc68acc224912b5ddbeca29779b
kukaro <justkukaro@naver.com>
2018-10-17 12:18:37 +0900

    last wpqkf

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# cd BackEnd-Express
-# sudo npm install
-# cd ..
-# cd FrontEnd-Vue
-# sudo npm install
-# sudo npm run build
-# sudo rm -rf ../BackEnd-Express/public/static
-# sudo mv dist/static ../BackEnd-Express/public
-# sudo mv dist/index.html dist/index.ejs
-# sudo mv dist/index.ejs ../BackEnd-Express/views
+sudo chmod -R 777 .
+cd BackEnd-Express
+sudo npm install
+cd ..
+cd FrontEnd-Vue
+sudo npm install
+sudo npm run build
+sudo rm -rf ../BackEnd-Express/public/static
+sudo mv dist/static ../BackEnd-Express/public
+sudo mv dist/index.html dist/index.ejs
+sudo mv dist/index.ejs ../BackEnd-Express/views

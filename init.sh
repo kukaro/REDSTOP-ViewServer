44bab04507588226bdf0417e567738989208c72f
kukaro <justkukaro@naver.com>
2018-10-17 12:21:57 +0900

    why

--- init.sh
+++ init.sh
@@ -2,10 +2,14 @@
 sudo chmod -R 777 .
 cd BackEnd-Express
 sudo npm install
+sudo chmod -R 777 .
 cd ..
+sudo chmod -R 777 .
 cd FrontEnd-Vue
 sudo npm install
+sudo chmod -R 777 .
 sudo npm run build
+sudo chmod -R 777 .
 sudo rm -rf ../BackEnd-Express/public/static
 sudo mv dist/static ../BackEnd-Express/public
 sudo mv dist/index.html dist/index.ejs

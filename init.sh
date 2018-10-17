6da988753cbf1ea9495c1a8d20a9a20ececde6a6
kukaro <justkukaro@naver.com>
2018-10-17 12:04:07 +0900

    test

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd BackEnd-Express
-sudo npm install
+# sudo npm install
 cd ..
 cd FrontEnd-Vue
 sudo npm install
-sudo npm run build
-sudo rm -rf ../BackEnd-Express/static
+# sudo npm run build
+sudo rm -rf ../BackEnd-Express/public/static
 sudo mv dist/static ../BackEnd-Express/public
 sudo mv dist/index.html dist/index.ejs
 sudo mv dist/index.ejs ../BackEnd-Express/views

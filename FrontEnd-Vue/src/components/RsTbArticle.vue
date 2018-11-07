ec03b3003f5cc88037bf5d28482d9ece7a000f36
kukaro <justkukaro@naver.com>
2018-11-08 06:25:36 +0900

    test

--- FrontEnd-Vue/src/components/RsTbArticle.vue
+++ FrontEnd-Vue/src/components/RsTbArticle.vue
@@ -262,7 +262,7 @@
               if (data[idx].parentBlockId === null) {
                 navTree.push(data[idx])
                 data[idx].children = []
-                let tmp = data[idx]
+                let tmp = 1
                 q.enq(tmp)
                 console.log(q)
                 // data.splice(idx, 1)

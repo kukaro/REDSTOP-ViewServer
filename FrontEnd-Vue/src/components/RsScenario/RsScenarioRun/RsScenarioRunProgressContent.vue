14998c9fd0b3bc1d0a3cec4354cb745f6343d31f
easyfordev <easyfordev8@gmail.com>
2018-11-19 03:04:52 +0900

    Charts 라인 5개

--- FrontEnd-Vue/src/components/RsScenario/RsScenarioRun/RsScenarioRunProgressContent.vue
+++ FrontEnd-Vue/src/components/RsScenario/RsScenarioRun/RsScenarioRunProgressContent.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="rs-scenario-run-progress-content">
-    <canvas id="mycanvas2" count="2"></canvas>
-    <chartjs-line :labels="labels" :datalabel="'TPS'" :data="dataset" :bind="true" :backgroundcolor="'#4966eb'" :bordercolor="'#4966eb'" target="mycanvas2"></chartjs-line>
-    <chartjs-line :labels="labels" :datalabel="'Response Time'" :data="dataset2" target="mycanvas2"></chartjs-line>
+    <canvas id="mycanvas2" count="5"></canvas>
+    <chartjs-line :labels="labels" :datalabel="'TPS'" :data="dataset" :bind="true" :backgroundcolor="'#4966eb'" :bordercolor="'#4966eb'" :pointborderwidth="5" :pointbordercolor="'#4966eb'" target="mycanvas2"></chartjs-line>
+    <chartjs-line :labels="labels" :datalabel="'Response Time'" :data="dataset2" :backgroundcolor="'#2bbee8'" :bordercolor="'#2bbee8'" :pointborderwidth="5" :pointbordercolor="'#2bbee8'" target="mycanvas2"></chartjs-line>
+    <chartjs-line :labels="labels" :datalabel="'VUs'" :data="dataset3" :backgroundcolor="'#77c61f'" :bordercolor="'#77c61f'" :pointborderwidth="5" :pointbordercolor="'#77c61f'" :pointbackgroundcolor="'#77c61f'" target="mycanvas2"></chartjs-line>
+    <chartjs-line :labels="labels" :datalabel="'Failures'" :data="dataset4" :backgroundcolor="'#eb9e49'" :bordercolor="'#eb9e49'" :pointborderwidth="5" :pointbordercolor="'#eb9e49'" :pointbackgroundcolor="'#eb9e49'" target="mycanvas2"></chartjs-line>
+    <chartjs-line :labels="labels" :datalabel="'Failures/s'" :data="dataset5" :backgroundcolor="'#ebd249'" :bordercolor="'#ebd249'" :pointborderwidth="5" :pointbordercolor="'#ebd249'" :pointbackgroundcolor="'#ebd249'" target="mycanvas2"></chartjs-line>
+
     <button id="btn-run" @click="gotoreport"><span>결과보기</span></button>
   </div>
 </template>
@@ -24,6 +28,9 @@ export default {
         .then(response => {
           this.dataset.push(response.data.number)
           this.dataset2.push(response.data.number2)
+          this.dataset3.push(response.data.number3)
+          this.dataset4.push(response.data.number4)
+          this.dataset5.push(response.data.number5)
         });
       this.count++
     }
@@ -38,7 +45,10 @@ export default {
       timer: '',
       labels: ['00:00', '00:05', '00:10', '00:15', '00:20', '00:25', '00:30'],
       dataset: [],
-      dataset2: []
+      dataset2: [],
+      dataset3: [],
+      dataset4: [],
+      dataset5: []
     }
   }
 }

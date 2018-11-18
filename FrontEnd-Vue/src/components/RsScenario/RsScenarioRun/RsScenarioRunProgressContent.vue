bf287a17065adb19461c99e72f29db0401662b32
easyfordev <easyfordev8@gmail.com>
2018-11-19 01:06:53 +0900

    Graph mount timming 수정

--- FrontEnd-Vue/src/components/RsScenario/RsScenarioRun/RsScenarioRunProgressContent.vue
+++ FrontEnd-Vue/src/components/RsScenario/RsScenarioRun/RsScenarioRunProgressContent.vue
@@ -14,14 +14,17 @@
       <!--:pointhoverbackgroundcolor="'#000000'"-->
       <!--:pointhoverbordercolor="'#ffd663'">-->
     <!--</chartjs-line>-->
-    <canvas id="mycanvas" count="1"></canvas>
-    <chartjs-line
-      :labels="labels" :data="dataset1" :bind="true" :datalabel="'TPS'"
-      :linetension="0" :bordercolor="'#4966eb'" :backgroundcolor="'#4966eb'"
-      :pointborderwidth="5" :pointbordercolor="'#4966eb'" :pointbackgroundcolor="'#4966eb'"
-      :pointhoverborderwidth="3" :pointhoverbackgroundcolor="'#4966eb'"
-      :pointhoverbordercolor="'#ffd663'"
-      target="mycanvas"></chartjs-line>
+    <chartjs-line :labels="mylabels" :datasets="mydatasets"></chartjs-line>
+
+    <!--<canvas id="mycanvas" count="1"></canvas>-->
+    <!--<chartjs-line-->
+      <!--:labels="labels" :data="dataset1" :datalabel="'TPS'" :bind="true"-->
+      <!--:linetension="0" :bordercolor="'#4966eb'" :backgroundcolor="'#4966eb'"-->
+      <!--:pointborderwidth="5" :pointbordercolor="'#4966eb'" :pointbackgroundcolor="'#4966eb'"-->
+      <!--:pointhoverborderwidth="3" :pointhoverbackgroundcolor="'#4966eb'"-->
+      <!--:pointhoverbordercolor="'#ffd663'"-->
+      <!--target="mycanvas"></chartjs-line>-->
+    <!--&lt;!&ndash;<button @click="runrun"><span>결과보기</span></button>&ndash;&gt;-->
     <!--<chartjs-line-->
       <!--:labels="labels" :data="dataset2" :bind="true" :datalabel="'Response Time'"-->
       <!--:linetension="0" :bordercolor="'#2bbee8'" :backgroundcolor="'#2bbee8'"-->
@@ -57,16 +60,21 @@
 </template>
 
 <script>
+/*eslint-disable*/
 export default {
   name: 'rs-scenario-run-progress-content',
   methods: {
     gotoreport () {
       this.$router.push({name: 'RsReport'})
       // this.$router.push({name: 'RsScenarioDetail', params: { name: item }})
+    },
+    runrun () {
+      this.dataset1.push(0)
     }
   },
   created: function () {
-    this.dataset1 = [12, 59, 80, 81, 56, 55, 40, 43, 15, 33]
+    this.dataset1 = [12, 59, 80, 81, 56, 55, 40]
+    this.dataset1.push(33)
   },
   data () {
     return {
@@ -75,8 +83,58 @@ export default {
       dataset2: [32, 9, 1, 33, 97, 11, 32, 55, 12, 7],
       dataset3: [86, 25, 28, 63, 39, 1, 22, 7, 97, 54],
       dataset4: [10, 10, 10, 10, 10, 7, 7, 10, 10, 7],
-      dataset5: [75, 54, 24, 56, 24, 86, 22, 44, 96, 30]
+      dataset5: [75, 54, 24, 56, 24, 86, 22, 44, 96, 30],
+      mylabels: ["January", "February", "March", "April", "May", "June", "July"],
+      mydatasets:[{
+        label: "My first dataset",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+        spanGaps: false,
+      },
+        {
+          label: "My second dataset",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [0, 20, 40, 41, 66, 25, 80],
+          spanGaps: false,
+        },]
     }
+  },
+  watch: {
+    // dataset1: function () {
+    //   // this.dataset1 =
+    // }
   }
 }
 </script>

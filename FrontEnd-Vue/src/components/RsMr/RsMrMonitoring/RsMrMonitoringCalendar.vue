0daf6f689a92b53f316f2434f4c580897c74b21a
“안지홍” <hongsam0102@gmail.com>
2018-11-19 21:57:34 +0900

    calendar 수정

--- FrontEnd-Vue/src/components/RsMr/RsMrMonitoring/RsMrMonitoringCalendar.vue
+++ FrontEnd-Vue/src/components/RsMr/RsMrMonitoring/RsMrMonitoringCalendar.vue
@@ -12,24 +12,24 @@
 
     <span>From</span>
     <select id="from" name="from">
-      <option value="7">october 12</option>
-      <option value="6">october 13</option>
-      <option value="5">october 14</option>
-      <option value="4">october 15</option>
-      <option value="3">october 16</option>
-      <option value="2">october 17</option>
-      <option value="1">october 18</option>
+      <option value="7">november 15</option>
+      <option value="6">november 16</option>
+      <option value="5">november 17</option>
+      <option value="4">november 18</option>
+      <option value="3">november 19</option>
+      <option value="2">november 20</option>
+      <option value="1">november 21</option>
     </select>
 
     <span>To</span>
     <select id="to" name="to">
-      <option value="7">october 18</option>
-      <option value="6">october 17</option>
-      <option value="5">october 16</option>
-      <option value="4">october 15</option>
-      <option value="3">october 14</option>
-      <option value="2">october 13</option>
-      <option value="1">october 12</option>
+      <option value="7">november 21</option>
+      <option value="6">november 20</option>
+      <option value="5">november 19</option>
+      <option value="4">november 18</option>
+      <option value="3">november 17</option>
+      <option value="2">november 16</option>
+      <option value="1">november 15</option>
     </select>
   </div>
 </template>

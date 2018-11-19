4436786304524b0996fee1a1e1518fbf1cfec846
kukaro <justkukaro@naver.com>
2018-11-19 09:57:51 +0900

    set

--- FrontEnd-Vue/src/components/RsTbAside/RsTbAsideGblock.vue
+++ FrontEnd-Vue/src/components/RsTbAside/RsTbAsideGblock.vue
@@ -29,6 +29,7 @@ export default {
     initCurrentBlock: function () {
       this.currentBlock = this.initCurrentBlock
       // console.log(this.currentBlock)
+      childBlocks: []
       let treeDataSerial = this.$store.state.app.treeDataSerial
       for(let atom of treeDataSerial){
         if(atom.parentBlockId === this.currentBlock.id){
